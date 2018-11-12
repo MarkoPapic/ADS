9c87b1a155dd22077f04d1beea878af5f7fbe9ae
Marko Papic <marko.papic@memini.co.uk>
2018-11-12 14:59:57 +0100

    Priority queue.

---
+++ ADS.DataStructures/PriorityQueue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADS.DataStructures
+{
+    public class PriorityQueue<T>
+    {
+        private readonly MinHeap<T> data;
+
+        public int Count => this.data.Count;
+
+        public PriorityQueue(Func<T, T, int> compareFunc)
+        {
+            data = new MinHeap<T>(compareFunc);
+        }
+
+        public void Enqueue(T element)
+        {
+            this.data.Insert(element);
+        }
+
+        public T Dequeue()
+        {
+            if (this.data.Count == 0)
+                throw new InvalidOperationException("Queue empty.");
+
+            return this.data.PopMin();
+        }
+
+        public T Peek()
+        {
+            if (this.data.Count == 0)
+                throw new InvalidOperationException("Queue empty.");
+
+            return this.data.PeekMin();
+        }
+    }
+}
---
+++ Tests/ADS.DataStructures.Tests/PriorityQueue/PriorityQueue_Dequeue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace ADS.DataStructures.Tests.PriorityQueue
+{
+    public class PriorityQueue_Dequeue
+    {
+        [Fact]
+        public void EmptyQueue_ExceptionThrown()
+        {
+            //Arrange
+            PriorityQueue<int> priorityQueue = new PriorityQueue<int>((a, b) => a.CompareTo(b));
+
+            //Act
+            Action act = () => priorityQueue.Dequeue();
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(act);
+        }
+
+        [Fact]
+        public void HappyPath_MinReturned()
+        {
+            //Arrange
+            int[] elements = new int[] { 1, -5, 24, 561, -300, 15, 36, 82, 4, 50 };
+            PriorityQueue<int> priorityQueue = new PriorityQueue<int>((a, b) => a.CompareTo(b));
+
+            foreach (int el in elements)
+                priorityQueue.Enqueue(el);
+
+            //Act && assert
+            int[] elementsLeft = new int[elements.Length];
+            elements.CopyTo(elementsLeft, 0);
+            for (int i = 0; i < elements.Length; i++)
+            {
+                int actualMinEl = elementsLeft.Min();
+                int minEl = priorityQueue.Dequeue();
+                Assert.Equal(actualMinEl, minEl);
+                elementsLeft = elementsLeft.Where(x => x != actualMinEl).ToArray();
+            }
+        }
+
+        [Fact]
+        public void HappyPath_CountUpdated()
+        {
+            //Arrange
+            int[] elements = new int[] { 1, -5, 24, 561, -300, 15, 36, 82, 4, 50 };
+            PriorityQueue<int> priorityQueue = new PriorityQueue<int>((a, b) => a.CompareTo(b));
+
+            foreach (int el in elements)
+                priorityQueue.Enqueue(el);
+
+            //Act
+            int minEl = priorityQueue.Dequeue();
+
+            //Assert
+            Assert.Equal(9, priorityQueue.Count);
+        }
+    }
+}
