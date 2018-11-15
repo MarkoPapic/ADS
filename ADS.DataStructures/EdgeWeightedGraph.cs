9f2674186741544890ea70e9dd5753b0c6b8fb09
Marko Papic <marko.papic@memini.co.uk>
2018-11-15 19:45:05 +0100

    Edge-weighted graph.

---
+++ ADS.DataStructures/EdgeWeightedGraph.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADS.DataStructures
+{
+    public class EdgeWeightedGraph
+    {
+        private readonly int V;
+        private readonly List<Edge>[] adj;
+
+        public EdgeWeightedGraph(int V)
+        {
+            this.V = V;
+            this.adj = new List<Edge>[V];
+            for (int i = 0; i < V; i++)
+                adj[i] = new List<Edge>();
+        }
+
+        public void AddEdge(Edge e)
+        {
+            int v = e.Either();
+            int w = e.Other(v);
+
+            ThrowIfOutOfRange(v);
+            ThrowIfOutOfRange(w);
+
+            this.adj[v].Add(e);
+            this.adj[w].Add(e);
+        }
+
+        public IEnumerable<Edge> Adj(int v)
+        {
+            ThrowIfOutOfRange(v);
+
+            return this.adj[v];
+        }
+
+        private void ThrowIfOutOfRange(int index)
+        {
+            if (index < 0 || index >= this.V)
+                throw new IndexOutOfRangeException();
+        }
+    }
+
+    public class Edge : IComparable<Edge>
+    {
+        private readonly int v;
+        private readonly int w;
+        private readonly double weight;
+
+        public Edge(int v, int w, double weight)
+        {
+            this.v = v;
+            this.w = w;
+            this.weight = weight;
+        }
+
+        public int Either()
+        {
+            return v;
+        }
+
+        public int Other(int vertex)
+        {
+            if (vertex == v)
+                return w;
+            return v;
+        }
+
+        public int CompareTo(Edge other)
+        {
+            if (this.weight < other.weight)
+                return -1;
+            else if (this.weight > other.weight)
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
