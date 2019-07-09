6ae356ecbe1b1ef53541f322067888cc373d54b0
Marko Papic <marko@markopapic.com>
2019-07-09 15:06:51 +0200

    Graph topological sort.

---
+++ ADS.Algorithms/Graphs/TopologicalSortDfs.cs
@@ -0,0 +1,33 @@
+﻿using ADS.DataStructures;
+using System.Collections.Generic;
+
+namespace ADS.Algorithms.Graphs
+{
+    public class TopologicalSortDfs
+    {
+        private bool[] visited;
+        private LinkedList<int> reversePostOrder;
+
+        public TopologicalSortDfs(Digraph graph)
+        {
+            reversePostOrder = new LinkedList<int>();
+            //We avoid using index 0, because we save it for special cases
+            visited = new bool[graph.V + 1];
+            //We need to iterate in case graph is not connected. This way we will traverse the whole graph.
+            for (int v = 1; v <= graph.V; v++)
+                if (!visited[v])
+                    Dfs(graph, v);
+        }
+
+        public IEnumerable<int> Order() => reversePostOrder;
+
+        private void Dfs(Digraph graph, int v)
+        {
+            visited[v] = true;
+            foreach (int w in graph.Adj(v))
+                if (!visited[w])
+                    Dfs(graph, w);
+            reversePostOrder.AddFirst(v);
+        }
+    }
+}
---
+++ Tests/ADS.Algorithms.UnitTests/TopologicalSortDfs_Order.cs
@@ -0,0 +1,28 @@
+﻿using ADS.Algorithms.Graphs;
+using ADS.DataStructures;
+using System.Linq;
+using Xunit;
+
+namespace ADS.Algorithms.UnitTests
+{
+    public class TopologicalSortDfs_Order
+    {
+        [Fact]
+        public void HappyPath_OrderedCorrectly()
+        {
+            //Arrange
+            (int, int)[] edges = new (int, int)[] { (1, 2), (1, 6), (1, 3), (2, 5), (4, 7), (4, 3), (4, 6), (4, 5), (6, 3), (7, 1), (7, 5) };
+            int[] expectedOrder = new int[] { 4, 7, 1, 6, 3, 2, 5 };
+            Digraph digraph = new Digraph(7);
+            foreach (var edge in edges)
+                digraph.AddEdge(edge.Item1, edge.Item2);
+
+            //Act
+            TopologicalSortDfs ts = new TopologicalSortDfs(digraph);
+            int[] topologicalOrder = ts.Order().ToArray(); ;
+
+            //Assert
+            Assert.True(expectedOrder.SequenceEqual(topologicalOrder));
+        }
+    }
+}
