dfa34e2b01405e74b04c4c2245039d6db00dfdbe
Marko Papic <marko@markopapic.com>
2019-07-28 16:06:03 +0200

    R-Way Trie.

---
+++ Tests/ADS.UnitTests/DataStructures/TrieRWay/TrieRWay_Get.cs
@@ -0,0 +1,45 @@
+﻿using ADS.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ADS.UnitTests.DataStructures.TrieRWay
+{
+    public class TrieRWay_Get
+    {
+        [Fact]
+        public void HappyPath_ElementRetuned()
+        {
+            //Arrange
+            (string, int)[] elements = new (string, int)[] { ("one", 1), ("five", 5), ("nine", 9), ("two", 2), ("three", 3), ("six", 6), ("four", 4), ("seven", 7), ("eight", 8) };
+            TrieRWay<int> trie = new TrieRWay<int>();
+            foreach (var el in elements)
+                trie.Insert(el.Item1, el.Item2);
+
+            //Act & Assert
+            foreach (var el in elements)
+            {
+                int foundEl = trie.Get(el.Item1);
+                Assert.Equal(el.Item2, foundEl);
+            }
+        }
+
+        [Fact]
+        public void KeyNotPresent_ExceptionThrown()
+        {
+            //Arrange
+            string nonExistingKey = "something";
+            (string, int)[] elements = new (string, int)[] { ("one", 1), ("five", 5), ("nine", 9), ("two", 2), ("three", 3), ("six", 6), ("four", 4), ("seven", 7), ("eight", 8) };
+            TrieRWay<int> trie = new TrieRWay<int>();
+            foreach (var el in elements)
+                trie.Insert(el.Item1, el.Item2);
+
+            //Act
+            Action act = () => trie.Get(nonExistingKey);
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(act);
+        }
+    }
+}
---
+++ Tests/ADS.UnitTests/DataStructures/TrieRWay/TrieRWay_Insert.cs
@@ -0,0 +1,48 @@
+﻿using ADS.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ADS.UnitTests.DataStructures.TrieRWay
+{
+    public class TrieRWay_Insert
+    {
+        [Fact]
+        public void ElementInserted()
+        {
+            //Arrange
+            (string, int)[] elements = new (string, int)[] { ("one",1), ("five",5), ("nine", 9), ("two", 2), ("three", 3), ("six", 6), ("four", 4), ("seven", 7), ("eight", 8) };
+            TrieRWay<int> trie = new TrieRWay<int>();
+
+            //Act
+            foreach (var el in elements)
+                trie.Insert(el.Item1, el.Item2);
+
+            //Assert
+            foreach (var el in elements)
+            {
+                bool exists = trie.ContainsKey(el.Item1);
+                Assert.True(exists);
+            }
+        }
+
+        [Fact]
+        public void NoDuplicates()
+        {
+            //Arrange
+            (string, int) duplicate = ("nine", 24);
+            (string, int)[] elements = new (string, int)[] { ("one", 1), ("five", 5), ("nine", 9), ("two", 2), ("three", 3), ("six", 6), ("four", 4), ("seven", 7), ("eight", 8) };
+            TrieRWay<int> trie = new TrieRWay<int>();
+
+            //Act
+            foreach (var el in elements)
+                trie.Insert(el.Item1, el.Item2);
+            trie.Insert(duplicate.Item1, duplicate.Item2);
+
+            //Assert
+            int found = trie.Get(duplicate.Item1);
+            Assert.Equal(duplicate.Item2, found);
+        }
+    }
+}
