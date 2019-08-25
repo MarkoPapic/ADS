210c77f759ae3de29ef7ebe63c21c0f8b12a1d2b
Marko Papic <marko@markopapic.com>
2019-08-25 17:57:20 +0200

    Weighted quick-union.

---
+++ ADS/Algorithms/DynamicConnectivity/WeightedQuickUnion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADS.Algorithms.DynamicConnectivity
+{
+    public class WeightedQuickUnion : UnionFind
+    {
+        // size of component for roots
+        private int[] size;
+
+        public WeightedQuickUnion(int n) : base(n)
+        {
+            size = new int[n];
+            for (int i = 0; i < n; i++)
+                size[i] = 1;
+        }
+
+        public override int Find(int p)
+        {
+            while (p != id[p])
+                p = id[p];
+            return p;
+        }
+
+        public override void Union(int p, int q)
+        {
+            int i = Find(p);
+            int j = Find(q);
+            if (i == j)
+                return;
+
+            // Make smaller root point to larger one.
+            if (size[i] < size[j])
+            {
+                id[i] = j;
+                size[j] += size[i];
+            }
+            else
+            {
+                id[j] = i;
+                size[i] += size[j];
+            }
+            _count--;
+        }
+    }
+}
