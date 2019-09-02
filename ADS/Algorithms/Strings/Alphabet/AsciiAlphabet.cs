59532d7a60743957dad6dd3e87894d2cb4fe4a80
Marko Papic <marko@markopapic.com>
2019-09-02 12:59:12 +0200

    KMP substring search.

---
+++ ADS/Algorithms/Strings/Alphabet/AsciiAlphabet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADS.Algorithms.Strings
+{
+    public class AsciiAlphabet : IAlphabet
+    {
+        public int R => 256;
+
+        public char ToChar(int i)
+        {
+            return (char)i;
+        }
+
+        public int ToIndex(char c)
+        {
+            return (int)c;
+        }
+    }
+}
---
+++ ADS/Algorithms/Strings/Alphabet/IAlphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADS.Algorithms.Strings
+{
+    public interface IAlphabet
+    {
+        int R { get; }
+        char ToChar(int i);
+        int ToIndex(char c);
+    }
+}
